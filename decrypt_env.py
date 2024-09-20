14e8775adc20f026e0b570a80b4361c576df29d5
NiceTryJony <gol.daniel30@gmail.com>
2024-09-20 15:08:43 +0200

    make it better

--- decrypt_env.py
+++ decrypt_env.py
@@ -1,9 +1,9 @@
 
-#(import) subprocess
+# )import subprocess
 
-#def decrypt_env_gpg():
- #   try:
-  #      subprocess.run(['gpg', '--decrypt', '--output', '.env', '.env.gpg'], check=True)
-   # except subprocess.CalledProcessError as e:
-    #    print(f"Error decrypting .env.gpg: {e}")
-     #   raise
+# def decrypt_env_gpg():
+#         try:
+#             subprocess.run(['gpg', '--decrypt', '--output', '.env', '.env.gpg'], check=True)
+#             except subprocess.CalledProcessError as e:
+#             print(f"Error decrypting .env.gpg: {e}")
+#         raise
--- telegram_web.py
+++ telegram_web.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
@@ -15,4 +15,4 @@ def user_data():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
